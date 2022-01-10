61b8cab329c18d0ebdff37de66f4fb77140610b5
Sai Kiran Karampudi <saikiran.k@outlook.in>
2022-01-10 11:11:35 +0100

    Updated config test file

--- api/__tests__/config.test.js
+++ api/__tests__/config.test.js
@@ -10,4 +10,16 @@ describe('Validate Configuration file', () => {
         let port = config.getPort();
         expect(port).not.toBe(undefined);
     })
+    it('should validate Front-end URL', () => {
+        let frontEndURL = config.getFrontEndURL();
+        expect(frontEndURL).not.toBe(undefined);
+    })
+    it('should validate User', () => {
+        let user = config.getUser();
+        expect(user).not.toBe(undefined);
+    })
+    it('should validate Password', () => {
+        let pwd = config.getPwd();
+        expect(pwd).not.toBe(undefined);
+    })
 })
